e95252a608fd60a9225020f1474fd8f068a4b907
HendZaineddin <hendzad.32@gmail.com>
2023-08-10 14:06:24 -0700

    c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,8 @@
 
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle\n");
+	/* new comment */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
-{
-	/* first C program */
-}	
