f5135254ea0eed80544e565f55a07de294a6bb83
HendZaineddin <hendzad.32@gmail.com>
2023-08-10 14:34:29 -0700

    c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/* main "Programming is like building a multilingual puzzle */
+
+/**
+ * main - print "Programming is like building a multilingual
+ *  puzzle, followed by a new line.
+ *  return 0.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
