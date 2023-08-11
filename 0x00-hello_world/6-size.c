7ed6592f1029dfd809e33c6ee2c8b1ebc46e4c42
HendZaineddin <hendzad.32@gmail.com>
2023-08-10 17:49:36 -0700

    c project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ int main(void)
 	printf("Size of a long int: lu% byte(s)\n", sizeof(c));
 	printf("Size of a long long int: lu% byte(s)\n", sizeof(d));
 	printf("Size of a float: lu% byte(s)\n", sizeof(e));
-	retun(0)
+	retun(0);
 }
