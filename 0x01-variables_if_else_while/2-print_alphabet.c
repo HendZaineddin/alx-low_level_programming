f0b50d528ac7e20619cc34631e273cb151ea3796
HendZaineddin <hendzad.32@gmail.com>
2023-08-11 15:48:26 -0700

    lol

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ /**
+  * main - Entry point
+  * Description: print the alphabet in lowercase
+  * Return: 0 (sucses).
+  */
+int main(void)
+{
+	char alphapet;
+
+	for (alphapet = 'a'; alphapet <= 'z'; alphapet++)
+	{
+		putchar(alphapet);
+	}
+
+	putchar('\n');
+	return (0);
+}
