b3b67813377725414539b2437d7c9774e96eec3b
HendZaineddin <hendzad.32@gmail.com>
2023-08-15 15:34:36 -0700

    alx

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - function check lowercase.
+ * @c: checks input.
+ * Return: return 1 if 'c' is lowercase otherwise always 0.
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
