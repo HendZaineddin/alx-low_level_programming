f6324daa6576f2e52e0387a2f5030920227d3d52
HendZaineddin <hendzad.32@gmail.com>
2023-08-15 16:51:28 -0700

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function print sign.
+ * @n : checks input.
+ * Return: return 1 if n > 0 and return 0 if n == 0 and return -1 if n < 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
