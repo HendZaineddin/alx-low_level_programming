657322017bb2e01eddc07c7e40d1c1c6241c4791
HendZaineddin <hendzad.32@gmail.com>
2023-08-21 16:46:15 -0700

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+int i, g;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+
+g = i - 1;
+for ( ; g >= 0; g--)
+{
+putchar(s[g]);
+}
+putchar('\n');
+}
