93848877adacddaceae85ae1b88b0a0ddab25bac
HendZaineddin <hendzad.32@gmail.com>
2023-08-21 15:31:04 -0700

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+	   int i, g;
+
+   for (i = 0; s[i] = '\0'; i++)
+   
+       g = i - 1;
+       for (g = i - 1; s[g] = '\0'; g--)
+       {
+           putchar(s[g]);
+       }
+        putchar('\n');
+}
