da76ff1edfdff1fec362acbe12bf85776e8e6857
HendZaineddin <hendzad.32@gmail.com>
2023-08-22 10:50:35 -0700

    l

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,12 @@ int i, g;
 char bad;
 
 	for (i = 0; s[i] != '\0'; i++)
+		;
 
-		for (g = 0; g < i / 2; g++)
+		for (g = 0; g < i / 2 ; g++)
 		{
 			bad = s[g];
-			s[g] = s[i - g - 1];
-			s[i - g - 1] = bad;
+			s[g] = s[i - 1 - g];
+			s[i - 1 - g] = bad;
 		}
 }
