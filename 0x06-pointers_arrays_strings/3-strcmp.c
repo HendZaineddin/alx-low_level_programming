63ca4aa0f75da43e7835de3687247a35594f5501
HendZaineddin <hendzad.32@gmail.com>
2023-08-23 14:54:35 -0700

    l

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: The first comparison string.
+ * @s2: The second comparison string.
+ * Return: (*s1 - *s2)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
