0633057a2d5bd07faa8c4d6ec79869fac79d0832
HendZaineddin <hendzad.32@gmail.com>
2023-08-29 15:16:36 -0700

    l

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,10 +16,9 @@ for (i = 0; haystack[i] != '\0'; i++)
 if (haystack[i] == needle[0])
 {
 for (m = 0; (haystack[i + m] == needle[m]) && needle[m] != '\0'; m++)
-{
+
 pt = &haystack[i];
 return (pt);
-}
 }
 }
 return (0);
