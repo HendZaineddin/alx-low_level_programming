5cccc677802a01a79c7f0f76bcf4f00f78752430
HendZaineddin <hendzad.32@gmail.com>
2023-08-30 13:45:48 -0700

    l

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,13 @@ void _puts_recursion(char *s)
 {
 int i = 0;
 
-_putchar(s[i]);
-
-if (s[i] > '\0')
+if (s[i] == '\0')
 {
-_puts_recursion(s + 1);
+_putchar('\n');
 }
 else
-_putchar('\n');
+{
+_putchar(s[i]);
+_puts_recursion(s + 1);
+}
 }
