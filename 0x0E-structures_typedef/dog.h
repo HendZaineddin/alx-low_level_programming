3c16bba14734460549840d60b20549c0f4bfc161
HendZaineddin <hendzad.32@gmail.com>
2023-09-12 18:59:16 -0700

    l

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's struct
+ * @name:the name of dog
+ * @age: the age
+ * @owner: the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
