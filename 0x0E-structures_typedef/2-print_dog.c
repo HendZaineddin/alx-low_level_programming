db1a44f43b2c788c6f602fdb2e84d5170be91def
HendZaineddin <hendzad.32@gmail.com>
2023-09-12 19:46:18 -0700

    l

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,11 +7,7 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == 0)
-	{
-		printf("(nil)");
-	}
-	else if (d != NULL)
+	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
