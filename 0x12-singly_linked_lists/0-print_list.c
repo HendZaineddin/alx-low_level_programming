4d2438b9e2254e3c359e5ec0f0ca4619e51cf446
hendzaineddin <hendzad.32@gmail.com>
2023-09-21 17:39:01 -0700

    l

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,8 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else if (h != NULL)
-		{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		}
 		i++;
 	}
 	return (i);
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_node -  a function that adds a new node at a list_t list.
+ * @head: pointer to head.
+ * @str: pointer to string.
+ * Return: ptrhead or NULL.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptrhead = malloc(sizeof(list_t));
+
+	if (ptrhead == NULL)
+	{
+		return (NULL);
+	}
+	ptrhead->str = strdup(str);
+		ptrhead->next = *head;
+		*head = ptrhead;
+		ptrhead->len = strlen(ptrhead->str);
+		return (ptrhead);
+	if (!ptrhead->str)
+	{
+		free(*head);
+		return (NULL);
+	}
+}
