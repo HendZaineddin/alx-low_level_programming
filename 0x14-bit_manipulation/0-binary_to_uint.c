53c410eb1ce2ee56f63af3d1b16cbe507badef20
hendzaineddin <hendzad.32@gmail.com>
2023-09-28 18:36:31 -0700

    l

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,24 +13,22 @@ unsigned int binary_to_uint(const char *b)
 	{
 		for (i = 0; b[i] != '\0'; i++)
 		{
-			if (b[i] == 1 || b[i] == 0)
+			if (b[i] == '1' || b[i] == '0')
 			{
-				if (b[i] == 1)
+				if (b[i] == '1')
 				{
-					for (l = i + 1; l > 0; l--)
+					for (l = (i + 1); l > 0; l--)
 					{
 						k *= 2;
 					}
+					m += k;
 				}
-			m += k;
-			return (m);
 			}
-			if (b[i] == 'a')
+			else
 			{
-				break;
-				return (0);
+			return (0);
 			}
 		}
 	}
-	return (0);
+	return (m);
 }
