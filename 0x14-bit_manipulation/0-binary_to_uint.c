19a2fca57641443b181e0599da45c20bfd468fbb
HendZaineddin <hendzad.32@gmail.com>
2023-12-16 09:50:21 -0800

    l

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *binary_to_uint -  a function that converts a binary number to an integer.
+ *@b - pointer to string.
+ *Return: 0 or dec.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, j, dec = 0;
+
+
+	for (j = 0; b[j] != '\0'; j++)
+		;
+	if (b == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (j--)
+		{
+			if ((b[j] != '1') && (b[j] != '0'))
+			{
+				return (0);
+			}
+			if (b[j] == '1')
+			{
+				dec += pwer(i);
+			}
+			i++;
+		}
+	}
+	return (dec);
+}
+
